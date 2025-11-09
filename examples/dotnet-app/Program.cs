9a58d601a09827685abc9a2721837f5ec557d74d
dimitryf <dimitryf@amdocs.com>
2025-11-09 22:16:43 +0200

    added dotnet and node apps

---
+++ examples/dotnet-app/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DotnetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET Console App!");
+        }
+    }
+}
