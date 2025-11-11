d9eaf09d43f906464d27607c6d8a1748192e5f70
dimitryf <dimitryf@amdocs.com>
2025-11-11 17:28:33 +0200

    HardCode container name

--- vars/deployContainer.groovy
+++ vars/deployContainer.groovy
@@ -5,14 +5,20 @@ def call(Map cfg = [:]) {
     docker.image('agileqa/jenkins-agent:multi-tool').inside('--group-add 991 -v /var/run/docker.sock:/var/run/docker.sock --add-host=host.docker.internal:host-gateway') {
         sh '''
           set -eux
-          docker rm -f ${containerName} || true
-          cid=$(docker run -d --cap-add NET_BIND_SERVICE --name ${containerName} -P ${image})
+          docker rm -f dotnet_sample_container || true
+        '''
+        sh "docker run -d --cap-add NET_BIND_SERVICE --name ${containerName} -P ${image}"
+        sh '''
           sleep 2
-          mapped=$(docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' ${containerName} || true)
+          mapped=$(docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' dotnet_sample_container || true)
           APP_URL="http://host.docker.internal:${mapped}"
           echo "APP_URL=${APP_URL}" | tee app_url.properties
+
           for i in {1..30}; do
-            if curl -fsS "${APP_URL}/health" >/dev/null 2>&1; then break; fi
+            if curl -fsS "${APP_URL}/health" >/dev/null 2>&1; then
+              echo "Health OK"
+              break
+            fi
             sleep 1
           done
         '''
