8b386627a1b132cc2b9c058ee3d373340fe8a276
dimitryf <dimitryf@amdocs.com>
2025-11-11 16:45:20 +0200

    Updated

--- vars/dotnetBuild.groovy
+++ vars/dotnetBuild.groovy
@@ -1,28 +1,26 @@
 def call(Map cfg = [:]) {
-    String image      = cfg.image ?: selectAgent('dotnet') // default to your multi-tool image
+    String image      = cfg.image ?: selectAgent('dotnet')
     String dockerArgs = cfg.dockerArgs ?: ''
-    List envVars      = cfg.envVars ?: [
-        "DOTNET_CLI_TELEMETRY_OPTOUT=1",
-        "DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1",
-        "DOTNET_CLI_HOME=/home/jenkins",
-        "HOME=/home/jenkins"
-    ]
-    String config     = cfg.configuration ?: (params.CONFIGURATION ?: 'Release')
-    String project    = cfg.project ?: (params.PROJECT ?: '')
-    String sln        = cfg.solution ?: (params.SOLUTION ?: '')
+    List envVars      = cfg.envVars ?: []
+    String config     = cfg.configuration ?: 'Release'
+    String project    = cfg.project ?: ''
+    String sln        = cfg.solution ?: ''
+    String workDir    = cfg.workDir ?: '.'
 
     withEnv(envVars) {
         withToolImage(image, dockerArgs) {
-            sh "dotnet --info"
-            if (sln?.trim()) {
-                sh "dotnet restore '${sln}'"
-                sh "dotnet build '${sln}' -c ${config} --no-restore"
-            } else if (project) {
-                sh "dotnet restore '${project}'"
-                sh "dotnet build '${project}' -c ${config} --no-restore"
-            } else {
-                sh "dotnet restore"
-                sh "dotnet build -c ${config} --no-restore"
+            dir(workDir) {
+                sh "dotnet --info"
+                if (sln?.trim()) {
+                    sh "dotnet restore '${sln}'"
+                    sh "dotnet build '${sln}' -c ${config} --no-restore"
+                } else if (project) {
+                    sh "dotnet restore '${project}'"
+                    sh "dotnet build '${project}' -c ${config} --no-restore"
+                } else {
+                    sh "dotnet restore"
+                    sh "dotnet build -c ${config} --no-restore"
+                }
             }
         }
     }
